b7ca8112d00314ac5700d5c3ad997445c7779e63
jbuzzanco <jcbuzzanco@gmail.com>
2017-09-07 13:14:29 -0400

    Gets images and data associated to display on page
    gets the images for a search result and displays, says if loading the
    images. also displays tags, user, comments, likes that is associated
    with the image

--- src/app/image-list/image-list.component.ts
+++ src/app/image-list/image-list.component.ts
@@ -8,14 +8,27 @@ import { ImageService } from '../shared/image.service';
 })
 export class ImageListComponent implements OnInit {
   images: any[];
+  imagesFound: boolean = false;
+  searching: boolean = false;
+
+  handleSuccess(data){
+    this.imagesFound = true;
+    this.images = data.hits;
+    console.log(data.hits)
+  }
+
+  handleError(error){
+    console.log(error, "is error");
+  }
 
   constructor(private _imageService : ImageService) { }
 
   searchImages(query: string){
+    this.searching = true;
     return this._imageService.getImage(query).subscribe(
-      data => console.log(data, "is data"),
-      error => console.log (error, "is error"),
-      () => console.log("Request Complete")
+      data => this.handleSuccess(data),
+      error => this.handleError(error),
+      () => this.searching = false
     )
   }
 
